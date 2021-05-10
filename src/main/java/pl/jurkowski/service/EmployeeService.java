06a4739bb8629b18395272a805e30fe731482a2d
konrad241198 <konrad.jurkowski11@gmail.com>
2021-05-10 14:03:42 +0200

    employeemanager-backend

---
+++ src/main/java/pl/jurkowski/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package pl.jurkowski.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.jurkowski.exception.UserNotFoundException;
+import pl.jurkowski.model.Employee;
+import pl.jurkowski.repo.EmployeeRepo;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+}
